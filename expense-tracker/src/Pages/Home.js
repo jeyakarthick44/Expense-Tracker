c003282288caad1eabc0e2c2168cc2bd16e2efd8
jeyakarthick44 <jeyakarthick408@gmail.com>
2021-11-28 00:15:56 +0530

    folder structure created

---
+++ expense-tracker/src/Pages/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div className="home">
+            <div>TopFold</div>
+            <div>ExpenseList</div>
+        </div>
+    )
+}
+
+export default Home;
---
+++ expense-tracker/src/redux/reducers/expenses.js
@@ -0,0 +1,10 @@
+const initialState = {
+    expenseList:[],
+}
+
+export const expenseReducer=(state=initialState,actions)=>{
+    switch(actions.type){
+        default:
+            return state;
+    }
+};
